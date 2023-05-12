f7653277091bb8c20a26c3cfb5c7f6244b29fb5a
choi myeong yun <chmy2257@gmail.com>
2023-05-12 17:53:38 +0900

    components amount

---
+++ src/component/HolidayAbout.jsx
@@ -0,0 +1,10 @@
+const HolidayAbout = () => {
+    return (
+        <div className="holiday_about">
+            <h1>휴가 조회Com</h1>
+        </div>
+    );
+}
+
+
+export default HolidayAbout;
---
+++ src/component/HolidayApproval.jsx
@@ -0,0 +1,10 @@
+const HolidayApproval = () => {
+    return (
+        <div className="holiday_approval">
+            <h1>휴가 조회Com</h1>
+        </div>
+    );
+}
+
+
+export default HolidayApproval;
