55b493baf457ad84653988cbe51d64bc44d8a6e4
choi myeong yun <chmy2257@gmail.com>
2023-05-15 18:31:35 +0900

    modal component animation

--- src/component/HolidayManagement.jsx
+++ src/component/HolidayManagement.jsx
@@ -1,20 +1,20 @@
 import { useState } from 'react'
 import '../css/management.scss'
-import NewEmployee from '../modal/AddNewEmployee'
+import NewEmployeeModal from '../modal/AddNewEmployee'
 
 const HolidayManagement = () => {
 
     const [modalState, setModalState] = useState("modal_hide")
     const showModal = () => {
-        setModalState("modal_show")
+        setModalState(modalState === "modal_hide" ? "modal_show" : "modal_hide")
     }
 
     return (
-        <>        
-        <div className="holiday_management">
-            <p className="modal_btn" onClick={() => { showModal() }}>신규입사자 등록</p>
-        </div>
-        <NewEmployee modalState={modalState} />
+        <>
+            <div className="holiday_management">
+                <p className="modal_btn" onClick={() => { showModal() }}>신규입사자 등록</p>
+            </div>
+            <NewEmployeeModal modalState={modalState} showModal={showModal} />
         </>
 
     );
--- src/modal/AddNewEmployee.jsx
+++ src/modal/AddNewEmployee.jsx
@@ -1,33 +1,37 @@
 import '../css/management.scss'
 
-const NewEmployee = ({modalState}) => {
+const NewEmployeeModal = ({ modalState, showModal }) => {
     return (
-        <div className={`new_employee ${modalState}`}>
-            <h3>신규 입사자 등록하기</h3>
-            <ul>
-                <li>
-                    <p>직원이름</p>
-                </li>
-                <li>
-                    <input type="text" name="name" id="name" />
-                </li>
-                <li><p>입사일</p></li>
-                <li>
-                    <input type="date" name="joining_date" id="joining_date" />
-                </li>
-                <li>
-                    <p>연차</p>
-                </li>
-                <li>
-                    <input type="text" name="annual" id="annual" />
-                </li>
-                <li className="add_btn_box">
-                    <a href="#">등록하기</a>
-                </li>
-            </ul>
-        </div>
+        <>
+            <div className={`new_employee ${modalState}`}>
+                <h3>신규 입사자 등록하기</h3>
+                <ul>
+                    <li>
+                        <p>직원이름</p>
+                    </li>
+                    <li>
+                        <input type="text" name="name" id="name" />
+                    </li>
+                    <li><p>입사일</p></li>
+                    <li>
+                        <input type="date" name="joining_date" id="joining_date" />
+                    </li>
+                    <li>
+                        <p>연차</p>
+                    </li>
+                    <li>
+                        <input type="text" name="annual" id="annual" />
+                    </li>
+                    <li className="add_btn_box">
+                        <span className="madal_save_btn" onClick={()=>{showModal()}}>등록하기</span>
+                        <span className="modal_close_btn" onClick={()=>{showModal()}}>닫기</span>
+                    </li>
+                </ul>
+            </div>
+            <div id="modal_back" className={`modal_back  ${modalState}`} ></div>
+        </>
     );
 }
 
 
-export default NewEmployee;
+export default NewEmployeeModal;
